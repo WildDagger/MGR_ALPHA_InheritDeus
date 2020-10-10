29ee1ae4e47cb7c727d2577695321976028c8ba5
WildDagger Ho <hoyunxian@gmail.com>
2020-10-10 10:17:12 +0800

    初始化

---
+++ js/main.js
@@ -0,0 +1,34 @@
+$(function(){
+    var $modal = $('#modal')
+    var $modalPageContent = $('#modal .page-content')
+
+    function goToByScroll(id) {
+        // Remove "link" from the ID
+        id = id.replace("link", "");
+        // Scroll
+        $('html,body').animate({
+            scrollTop: $("#" + id).offset().top
+        }, 'slow');
+    }
+
+    $('.nav-main-header .nav-link').on('click', function(e) {
+        var link = $(this).data('link')
+        if (!(link != '' && link != undefined && link != null)) return
+
+        e.preventDefault()
+        goToByScroll(link)
+    })
+
+    $('#epList .list-group-item-action').on('click', function(e) {
+        e.preventDefault()
+
+        var $self = $(this)
+        $modalPageContent.load($self.data('link') + ' .scene', function() {
+            $modal.find('.modal-title').text($self.text())
+            $modal.modal({
+                backdrop: 'static',
+                keyboard: false
+            })
+        })
+    })
+})
